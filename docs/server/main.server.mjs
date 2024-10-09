bb69fc15047ab9aee307e5c7229ea7ac8d6ab662
بدر حسن مصطفى الجمال <badr.mustafa1701336@science.menofia.edu.eg>
2024-10-09 14:18:39 +0300

    Edite nav

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QE77MAWV.mjs";import"./chunk-CHIN4FRB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-37CAKDDL.mjs";import"./chunk-CHIN4FRB.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
